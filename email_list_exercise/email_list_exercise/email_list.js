21ec7e54d38e625ccf580050b66e3a62c879a1f8
vaultrl <73655721+vaultrl@users.noreply.github.com>
2021-01-28 22:55:45 -0700

    Add files via upload

---
+++ email_list_exercise/email_list_exercise/email_list.js
@@ -0,0 +1,67 @@
+const $ = selector => document.querySelector(selector);
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    $("#join_list").addEventListener("click", () => {
+        // get values user entered in textboxes
+        const email1 = $("#email_1");
+        const email2 = $("#email_2");
+        const firstName = $("#first_name");
+    
+        // create an error message and set it to an empty string
+        let isValid = true;
+
+        // check user entries - add text to error message if invalid
+        if (email1.value == "") {
+            email1.nextElementSibling.textContent = "This field is required.";
+            isValid = false; 
+        } 
+        
+        else {
+        
+            email1.nextElementSibling.textContent = "";
+        
+        }
+    
+        if (email2.value == "") { 
+
+            email2.nextElementSibling.textContent = "This field is required.";
+            isValid = false; 
+        } 
+        
+        else {
+            
+            email2.nextElementSibling.textContent = "";
+        }
+    
+        if (email1.value != email2.value) { 
+            email2.nextElementSibling.textContent = "Email addresses must match.";
+            isValid = false; 
+        }
+    
+
+        if (firstName.value == "") {
+            firstName.nextElementSibling.textContent = "First name is required.";
+            isValid = false;
+        } 
+        else 
+        {
+            firstName.nextElementSibling.textContent = "";
+        }
+    
+        // submit the form if error message is an empty string
+        if (isValid) {
+            $("#email_form").submit();
+        }
+    });
+
+    $("#clear_form").addEventListener("click", () => {
+        $("#email_1").value = "";
+        $("#email_2").value = "";
+        $("#first_name").value = "";
+
+        $("#email_1").focus();
+    });
+    
+    $("#email_1").focus();
+});
