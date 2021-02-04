e0325ff9a482fb67414a59cfed149871eba50fa5
vaultrl <73655721+vaultrl@users.noreply.github.com>
2021-02-03 21:26:01 -0700

    Add files via upload

--- slide_show/slide_show.js
+++ slide_show/slide_show.js
@@ -1,51 +1,23 @@
 "use strict";
-
-const $ = selector => document.querySelector(selector);
-    
-const imageCache = [];
-let imageCounter = 0;
-let timer = null;
-let image = null;
-
-const mainImage = $("#main_image");   // the img element for the show
-const caption = $("#caption");        // the h2 element for the caption
-
-const runSlideShow = function() {
-    imageCounter = (imageCounter + 1) % imageCache.length;
-    image = imageCache[imageCounter];
-    mainImage.src = image.src;
-    mainImage.alt = image.alt;
-    caption.textContent = image.alt;
-};
-         
-document.addEventListener("DOMContentLoaded", function(){
-    const links = $("#image_list").querySelectorAll("a");
-    
-    // process image links
-    for ( let link of links ) {
-        // Preload image and copy title properties
-        image = new Image();
-        image.src = link.href;
-        image.alt = link.title;
-
-        // add image to array 
-        imageCache[imageCache.length] = image;
-    }
-
-    // attach start and pause event handlers
-    $("#start").addEventListener("click", () => {
-
-        runSlideShow();
-        timer = setInterval(runSlideShow, 2000);
-        $("#start").disabled = true;
-        $("#pause").disabled = false;
-
-    });
-    $("#pause").addEventListener("click", () => {
+$(document).ready( () => {
+    let nextSlide = $("#slides img:first-child");
         
-        clearInterval(timer);
-        $("#start").disabled = false;
-        $("#pause").disabled = true;
-        
-    });
+    // start slide show
+    setInterval( () => {   
+        $("#caption").hide(1000);
+        $("#slide").slideUp(2000,
+            () => {
+                if (nextSlide.next().length == 0) {
+                    nextSlide = $("#slides img:first-child");
+                }
+                else {
+                    nextSlide = nextSlide.next();
+                }
+                const nextSlideSource = nextSlide.attr("src");
+                const nextCaption = nextSlide.attr("alt");
+                $("#slide").attr("src", nextSlideSource).slideDown(2000);                    
+                $("#caption").text(nextCaption).show(1000);
+            });    // end fadeOut()
+    },
+    5000);         // end setInterval()
 });
